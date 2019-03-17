059564f8802a248850ce40c1c8e9f6601619e811
AlPsvch <pasevich.aleksej@gmail.com>
2019-03-17 20:22:28 +0300

    Task 1.5 Added test to check return values of findProducts() method

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -42,4 +42,17 @@ public class ArrayListProductDaoTest {
     public void testGetProduct(){
         productDao.getProduct(1L);
     }
+
+    @Test
+    public void testFindProducts(){
+        Product product1 = new Product(1L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 0, imageURL);
+        Product product2 = new Product(2L, "sgs", "Samsung Galaxy S", null, usd, 100, imageURL);
+        Product product3 = new Product(3L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, imageURL);
+
+        productDao.save(product1);
+        productDao.save(product2);
+        productDao.save(product3);
+
+        assertEquals(1, productDao.findProducts().size());
+    }
 }
